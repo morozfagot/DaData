16450899a28ae6d3857498f932c965e0a4bbbf23
DESKTOP-7JR6UP3\Евгений <tidihanter@gmail.com>
2024-12-26 17:35:54 +0300

    исправил ошибки gitignore.

--- repos/DaData.Application/obj/Debug/net8.0/DaData.Application.AssemblyInfo.cs
+++ repos/DaData.Application/obj/Debug/net8.0/DaData.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DaData.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c865955c5f4882d634a8757536754132f3d92bb2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57686b99ef927c327a9fa3bee6ea73d7307c5d99")]
 [assembly: System.Reflection.AssemblyProductAttribute("DaData.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DaData.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
